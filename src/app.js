7579b350b09883fb179328eb9cc7ddb514f0f1a4
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2022-11-17 14:07:32 +0100

    try deploy

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 const { MongoClient } = require('mongodb')
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.qigutbk.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://test:GrG8VfNSJgddQ0wl@cluster0.qigutbk.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
